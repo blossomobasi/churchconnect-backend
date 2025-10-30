3a21fea79130af808928df1ee752a14c4a473448
Blossom Obasi <blossomobasi2@gmail.com>
2025-10-30 13:27:06 +0100

    Enhance main application bootstrap process with CORS support, versioning, and global validation/filtering

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,28 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
+import { CONFIGS } from "configs";
+import { ValidationPipe, VersioningType } from "@nestjs/common";
+import { AllExceptionFilter } from "./common/filters/all-exception.filter";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  const configService = app.get(ConfigService);
+    const configService = app.get(ConfigService);
 
-  const PORT = configService.get('CONFIGS.PORT');
-  const HOST = configService.get('CONFIGS.HOST');
+    app.enableCors({
+        credentials: true,
+        origin: [...CONFIGS.CORS_ALLOWED_ORIGINS],
+    });
+    app.enableVersioning({ type: VersioningType.URI });
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+    app.useGlobalFilters(new AllExceptionFilter());
 
-  await app.listen(PORT, HOST, () => {
-    console.log(`Server is running on http://${HOST}:${PORT}`);
-  });
+    const PORT = configService.get("CONFIGS.PORT");
+    const HOST = configService.get("CONFIGS.HOST");
+
+    await app.listen(PORT, HOST, () => {
+        console.log(`Server is running on http://${HOST}:${PORT}`);
+    });
 }
 bootstrap();
