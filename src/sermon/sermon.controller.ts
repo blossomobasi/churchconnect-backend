8a48562450ada1bcd281ae96a1542883ccf210ce
Blossom Obasi <blossomobasi2@gmail.com>
2025-12-21 04:50:14 +0100

    Implement route for updating and deleting a sermon

--- src/sermon/sermon.controller.ts
+++ src/sermon/sermon.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post, Query, Req, UploadedFiles, UseGuards, UseInterceptors } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query, Req, UploadedFiles, UseGuards, UseInterceptors } from "@nestjs/common";
 import { ApiBearerAuth, ApiConsumes, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { SermonService } from "./sermon.service";
 import { ApiHttpErrorResponses, ApiHttpResponse } from "src/common/decorators/custom-decorator";
@@ -16,6 +16,7 @@ import { validateFile } from "src/common/validators/file.validator";
 import { PaginationDto } from "src/common/dto/pagination.dto";
 import { GetAllSermonFilterDto } from "./dto/get-all-sermon-filter-dto";
 import { IPaginationMeta } from "src/common/utils/pagination";
+import { UpdateSermonDto } from "./dto/update-sermon-dto";
 
 @ApiTags("Sermons")
 @Controller({ path: "sermons", version: "1" })
@@ -87,5 +88,51 @@ export class SermonController {
     async getSermons(@Query() PaginationDto: PaginationDto, @Query() getAllSermonFilterDto: GetAllSermonFilterDto): Promise<HttpResponse<{ results: Sermon[]; meta: IPaginationMeta }>> {
         const sermons = await this.sermonService.getAllSermons(PaginationDto, getAllSermonFilterDto);
         return new HttpResponse("Sermons fetched successfully", sermons, HttpStatus.OK);
+    }
+
+    @ApiOperation({ summary: "Get sermon by ID" })
+    @ApiHttpErrorResponses()
+    @HttpCode(HttpStatus.OK)
+    @ApiHttpResponse({ status: HttpStatus.OK, type: CreateSermonDto, description: "Sermon fetched successfully" })
+    @ApiBearerAuth()
+    @UseGuards(JwtAuthGuard)
+    @Get(":sermonId")
+    async getSermonById(@Param("sermonId") sermonId: string): Promise<HttpResponse<Sermon>> {
+        const sermon = await this.sermonService.getSermonById(sermonId);
+        return new HttpResponse("Sermon fetched successfully", sermon, HttpStatus.OK);
+    }
+
+    @ApiOperation({ summary: "Update sermon by ID" })
+    @ApiConsumes("multipart/form-data")
+    @ApiHttpErrorResponses()
+    @HttpCode(HttpStatus.OK)
+    @ApiHttpResponse({ status: HttpStatus.OK, type: CreateSermonDto, description: "Sermon updated successfully" })
+    @ApiBearerAuth()
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.ADMIN, Role.DEPARTMENT_HEAD)
+    @UseInterceptors(
+        FileFieldsInterceptor([
+            { name: "audioFile", maxCount: 1 },
+            { name: "videoFile", maxCount: 1 },
+            { name: "thumbnailFile", maxCount: 1 },
+        ])
+    )
+    @Patch(":sermonId")
+    async updateSermonById(@Param("sermonId") sermonId: string, @Body() updateSermonDto: UpdateSermonDto, @UploadedFiles() files: { audioFile?: Express.Multer.File[]; videoFile?: Express.Multer.File[]; thumbnailFile?: Express.Multer.File[] }): Promise<HttpResponse<Sermon>> {
+        const sermon = await this.sermonService.updateSermon(sermonId, updateSermonDto, { audio: files.audioFile, video: files.videoFile, thumbnail: files.thumbnailFile });
+        return new HttpResponse("Sermon updated successfully", sermon, HttpStatus.OK);
+    }
+
+    @ApiOperation({ summary: "Delete sermon by ID" })
+    @ApiHttpErrorResponses()
+    @HttpCode(HttpStatus.OK)
+    @ApiHttpResponse({ status: HttpStatus.OK, type: Boolean, description: "Delete sermon description" })
+    @ApiBearerAuth()
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.ADMIN, Role.DEPARTMENT_HEAD)
+    @Delete(":sermonId")
+    async deleteSermonById(@Param("sermonId") sermonId: string): Promise<HttpResponse<void>> {
+        const sermon = await this.sermonService.deleteSermon(sermonId);
+        return new HttpResponse("Sermon deleted successfully", sermon, HttpStatus.NO_CONTENT);
     }
 }
